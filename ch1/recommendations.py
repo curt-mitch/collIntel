7dc88496d88013728c7fb6f03be348025c483877
Curtis Mitchell <curtis.l.mitchell@gmail.com>
2016-03-26 15:18:10 -0700

    Pearson correlation coefficient

--- ch1/recommendations.py
+++ ch1/recommendations.py
@@ -21,9 +21,9 @@ critics={'Lisa Rose': {'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5,
 from math import sqrt
 
 # Returns a distance-based similarity score for person1 and person2
-def sim_distance(prefs, person1, person2):
+def sim_distance (prefs, person1, person2):
   # Get the list of shared_items
-  si={}
+  si = {}
   for item in prefs[person1]:
     if item in prefs[person2]:
       si[item] = 1
@@ -35,3 +35,35 @@ def sim_distance(prefs, person1, person2):
                       for item in prefs[person1] if item in prefs[person2]])
 
   return 1 / (1 + sum_of_squares)
+
+def sim_pearson (prefs, person1, person2):
+  # Get the list of mutually rated items
+  si = {}
+  for item in prefs[person1]:
+    if item in prefs[person2]: si[item] = 1
+
+  # Find the number of elements
+  n = len(si)
+
+  # if there are no ratings in common, return 0
+  if n == 0: return 0
+
+  # Add up all the preferences
+  sum1 = sum([prefs[person1][it] for it in si])
+  sum2 = sum([prefs[person2][it] for it in si])
+
+  # Sum up the squares
+  sum1Sq = sum([pow(prefs[person1][it], 2) for it in si])
+  sum2Sq = sum([pow(prefs[person2][it], 2) for it in si])
+
+  # Sum up the products
+  pSum = sum([prefs[person1][it] * prefs[person2][it] for it in si])
+
+  # Calculate Pearson score
+  num = pSum - (sum1 * sum2 / n)
+  den = sqrt((sum1Sq - pow(sum1, 2) / n) * (sum2Sq - pow(sum2, 2) / n))
+  if den == 0: return 0
+
+  r = num / den
+
+  return r
